23ffe80a75af27816b0078550735b467fbaf67e7
Joakim Borgström <borgstro@kth.se>
2013-01-30 14:53:10 +0100

    Grund till entities

---
+++ src/java/Persistence/Person.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Persistence;
+
+/**
+ *
+ * @author Jocke
+ */
+public abstract class Person {
+    String name;
+    String surname;
+}
