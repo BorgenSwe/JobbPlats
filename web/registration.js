3a09259536810b980e24ff2a25d314ab880b36ab
Jonas Siltamäki Håkansson <jonassh@gmail.com>
2013-02-01 16:09:59 +0100

    Coolt formulär

---
+++ web/registration.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+   // Lägger in en bild på länkarna för att lägga till flera kompetenser/tillgängligheter
+   $('.addLink').each(function() {
+       $(this).html('<img src="resources/add.png" alt="Add Competence" />');
+   });
+   
+   // Kompetensrader
+   
+});
+
