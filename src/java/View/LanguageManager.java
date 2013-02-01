8cf91192b63f1708a0934e1e9ea874f79a152b59
Stefan <stefan.gerdin@gmail.com>
2013-02-01 12:35:51 +0100

    Internationalisering

---
+++ src/java/View/LanguageManager.java
@@ -0,0 +1,33 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package View;
+
+import java.util.Locale;
+import javax.annotation.ManagedBean;
+import javax.inject.Named;
+import javax.enterprise.context.Dependent;
+import javax.faces.context.FacesContext;
+
+/**
+ *
+ * @author 573w3
+ */
+@Named(value = "languageManager")
+@ManagedBean
+public class LanguageManager {
+
+    /**
+     * Creates a new instance of LanguageManager
+     */
+    public LanguageManager() {
+    }
+    
+    public void change() {
+        FacesContext.getCurrentInstance().getViewRoot().setLocale(new Locale("sv"));
+        //if (FacesContext.getCurrentInstance().getViewRoot().getLocale().getLanguage().equals(Locale.ENGLISH.getLanguage())) {
+        //    FacesContext.getCurrentInstance().getViewRoot().setLocale(new Locale("sv"));
+        //} else FacesContext.getCurrentInstance().getViewRoot().setLocale(Locale.ENGLISH);
+    }
+}
