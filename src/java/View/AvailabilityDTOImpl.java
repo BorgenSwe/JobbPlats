062642f554f8a656ed503f6dff0052ba47c2b627
Jonas Siltamäki Håkansson <jonassh@gmail.com>
2013-02-04 12:20:27 +0100

    parsa datum

--- src/java/View/AvailabilityDTOImpl.java
+++ src/java/View/AvailabilityDTOImpl.java
@@ -4,6 +4,8 @@
  */
 package View;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 /**
@@ -13,14 +15,28 @@ import java.util.Date;
 public class AvailabilityDTOImpl implements AvailabilityDTO {
     private Date from;
     private Date to;
+    
+    private Date parseDate(String str) {
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+        // Gör så att det måste vara exakt det angivna formatet
+        //format.setLenient(false); 
+        
+        try {
+            Date date = format.parse(str);
+            return date != null ? date : null;
+        } catch (ParseException ex) {
+            System.out.println("errorz");
+        }
+        return null;
+    }
 
     @Override
     public Date getFrom() {
 	return from;
     }
 
-    public void setFrom(Date from) {
-	this.from = from;
+    public void setFrom(String from) {
+        this.from = parseDate(from);
     }
 
     @Override
@@ -28,9 +44,7 @@ public class AvailabilityDTOImpl implements AvailabilityDTO {
 	return to;
     }
 
-    public void setTo(Date to) {
-	this.to = to;
+    public void setTo(String to) {
+	this.to = parseDate(to);
     }
-    
-    
 }
