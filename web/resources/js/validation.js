9c550e8e06bebd83486af5ee014aaa0bac5631a5
Jonas Siltamäki Håkansson <jonassh@gmail.com>
2013-02-06 13:41:36 +0100

    Jobbat på validering

--- web/resources/js/validation.js
+++ web/resources/js/validation.js
@@ -1,8 +1,10 @@
 function initValidation() {
-    // Regexps - FYLL I COOLA EXPRESSIONS!
-    var nameTest = new RegExp("");
-    var emailTest = new RegExp("");
-    var ssnTest = new RegExp("");
+    // Regexps
+    var nameTest = /[A-z -]+/g;
+    var emailTest = /[A-z0-9.-_]+@[A-z0-9.-_]+.[A-z.-_]+/g
+    var ssnTest = /[0-9]{10}/g;
+    var compTest = /[0-9]*.?[0-9]*/g;
+    var availTest = new RegExp("","g");//[0-9]{4}-[0-9]{2}-[0-9]{2}/g;
 
     $('.submitButton').click(function(e) {
        var surname = $('.surname').val()
@@ -12,26 +14,54 @@ function initValidation() {
 
        // Efternamn
        if (!nameTest.test(surname)) {
-           alert("FAIL: surname");
+           $('.surnameRow .errorColumn').fadeIn();
            e.preventDefault();
        }
        
        // Förnamn
        if (!nameTest.test(fname)) {
-           alert("FAIL: fname");
+           $('.fnameRow .errorColumn').fadeIn();
            e.preventDefault();
        }
 
        // Email
        if (!emailTest.test(email)) {
-           alert("FAIL: EMAIL");
+           $('.emailRow .errorColumn').fadeIn();
            e.preventDefault();
        }
        
        // Personnummer
        if (!ssnTest.test(ssn)) {
-           alert("FAIL: ssn");
+           $('.ssnRow .errorColumn').fadeIn();
            e.preventDefault();
        }
+       
+       // Kompetens
+       $('.competenceRow').each(function() {
+           var row = $(this);
+           if (row.css('display') != 'none') {
+               $(this).children('input').each(function(){
+                   var text = $(this).val();
+                   if (!compTest.test(text)) {
+                       row.children('.errorDiv').fadeIn();
+                       e.preventDefault();
+                   }
+               });
+           }
+       });
+       
+       // TIllgänglighet
+       $('.availabilityRow').each(function() {
+          var row = $(this);
+          if (row.css('display') != 'none') {
+              $(this).children('input').each(function(ix) {
+                 var text = $(this).val();
+                 if (!availTest.test(text)) {
+                    row.children('.errorDiv').fadeIn();
+                     e.preventDefault();
+                 }
+              });
+          } 
+       });
     });
 }
