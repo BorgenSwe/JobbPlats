35606ada955033754e6767441af2db22249ccc8a
Jonas Siltamäki Håkansson <jonassh@gmail.com>
2013-02-11 12:38:21 +0100

    Header och footer

--- web/resources/js/registration.js
+++ web/resources/js/registration.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
    
    // Lägger in en bild på länkarna för att lägga till flera kompetenser/tillgängligheter
    $('.addLink').each(function() {
-       $(this).html('<img src="resources/add-64.png" alt="Add Competence" width="20" height="20" />');
+       $(this).html('<img src="resources/gfx/add-64.png" alt="Add Competence" width="20" height="20" />');
    });
    
    /**
