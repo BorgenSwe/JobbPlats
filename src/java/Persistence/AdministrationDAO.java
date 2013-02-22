3106579822991497d88256173a5c8fcfb89718af
Joakim Borgström <borgstro@kth.se>
2013-02-22 15:22:28 +0100

    Börjat på AdministrationDAO

---
+++ src/java/Persistence/AdministrationDAO.java
@@ -0,0 +1,11 @@
+
+package Persistence;
+
+/**
+ * This DAO interface handles the data in and out to the database concerning the
+ * administration view.
+ * @author Joakim Borgström
+ */
+public interface AdministrationDAO {
+    
+}
